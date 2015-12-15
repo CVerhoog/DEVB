1cdd70948880237f6fc5b27e59c718ef5d4739e2
Corné Verhoog <corne-verhoog@hotmail.com>
2015-12-15 15:34:36 +0100

    commitDes

---
+++ DEVB.bat
@@ -0,0 +1,10 @@
+cd "C:\Users\Corné verhoog\Documents\GitHub\devA"
+@echo Checks Status of repository
+@echo adds changed files to commit
+git init
+git add *
+git commit -m "commitDes"
+@echo pushes commit to server
+git push
+
+pause
